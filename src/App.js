644f092de65bd5d0a5f3faf8613e91014e53b156
bugvlopper <bugvloppeur@gmail.com>
2022-05-22 17:55:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion";
+import React, { Component } from 'react';
+import box from "./images/illustration-box-desktop.svg";
+import mainImage from "./images/illustration-woman-online-desktop.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <React.Fragment>
+        <img className='box' src={box} alt="box" />
+      <div className='card'>
+        <div className='images-container'>
+          <img className='mainImage' src={mainImage} alt="woman working" />
+          
+        </div>
+        <div className='content'>
+          <h1>FAQ</h1>
+          <div className='accordion-container'>
+          <Accordion>
+          <h2>How many team members can I invite?</h2> 
+          <p>
+            You can invite up to 2 additional users on the Free plan. There is no limit on 
+            team members for the Premium plan.
+          </p>  
+          </Accordion>
+          <Accordion>
+          <h2>What is the maximum file upload size?</h2> 
+          <p>
+            No more than 2GB. All files in your account must fit your allotted storage space.
+          </p>  
+          </Accordion>
+          <Accordion>
+          <h2>How do I reset my password?</h2> 
+          <p>
+            Click “Forgot password” from the login page or “Change password” from your profile page.
+            A reset link will be emailed to you.
+          </p>  
+          </Accordion>
+          <Accordion>
+          <h2>Can I cancel my subscription?</h2> 
+          <p>
+            Yes! Send us a message and we’ll process your request no questions asked.
+          </p>  
+          </Accordion>
+          <Accordion>
+          <h2>Do you provide additional support?</h2> 
+          <p>
+            Chat and email support is available 24/7. Phone lines are open during normal business hours.
+          </p>  
+          </Accordion>
+          </div>
+        </div>
+      </div>
+
+      <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+      Coded by <a href="https://www.frontendmentor.io/profile/bugvlopper" target="_blank" rel='noreferrer'>Bugvlopper</a>.
     </div>
-  );
+      </React.Fragment>
+    
+    );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,58 @@
+import { Component } from "react";
+import "../css/Accordion.css"
+import arrow from "../images/icon-arrow-down.svg"
+
+class Accordion extends Component {
+    state = {  } 
+
+    _toggleActive(params){
+        console.log(params);
+        let target = params.target;console.log(target);
+        if(target.nodeName === "H2"){
+            if(target.parentElement.parentElement.className.match(/active/)){
+            target.parentElement.parentElement.classList.remove("active")
+          }else{
+            let lastActive = document.getElementsByClassName("content")[0].getElementsByClassName("active")
+            if (lastActive[0]) {
+              lastActive[0].classList.remove("active")
+            }
+            target.parentElement.parentElement.classList.add('active')
+          }
+        }else{
+            if(target.parentElement.className.match(/active/)){
+                target.parentElement.classList.remove("active")
+              }else{
+                let lastActive = document.getElementsByClassName("content")[0].getElementsByClassName("active")
+                if (lastActive[0]) {
+                  lastActive[0].classList.remove("active")
+                }
+                target.parentElement.classList.add('active')
+              }
+        }
+        
+    }
+
+    _hoverBoxMove(){
+        document.getElementsByClassName('box')[0].style.left = 19.5+"%";
+    }
+    
+    _hoverReplace(){
+         document.getElementsByClassName('box')[0].style.left = 21.2+"%";
+    }
+
+    render() { 
+        return (
+            <div className="accordion" >
+                <div className="faq-question" onClick={this._toggleActive} onMouseOver={this._hoverBoxMove} onMouseOut={this._hoverReplace}>
+                    {this.props.children[0]}
+                    <img src={arrow} alt="arrow" />
+                </div>
+                <div className="faq-response">
+                    {this.props.children[1]}
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default Accordion;
